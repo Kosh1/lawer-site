fffddfde88ce21f903c896dcbd53a0b22e2d99ba
Kosh1 <koshmanavt1@gmail.com>
2025-06-09 06:58:25 +0300

    Яндекс Метрика

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,21 +25,34 @@ export default function RootLayout({
         <link rel="manifest" href="/manifest.json" />
         <title>ИскИИ — AI генератор исковых заявлений</title>
         <!-- Yandex.Metrika counter -->
-        <script type="text/javascript" >
-          (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-          m[i].l=1*new Date();
-          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-          ym(102501372, "init", {
-                clickmap:true,
-                trackLinks:true,
-                accurateTrackBounce:true,
-                webvisor:true
-          });
-        </script>
-        <noscript><div><img src="https://mc.yandex.ru/watch/102501372" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+              ym(102501372, "init", {
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true
+              });
+            `,
+          }}
+        />
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/102501372"
+              style={{ position: "absolute", left: "-9999px;" }}
+              alt=""
+            />
+          </div>
+        </noscript>
         <!-- /Yandex.Metrika counter -->
       </head>
       <body>
