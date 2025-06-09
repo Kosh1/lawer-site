749b6af1b57dc754680d9af7b5a73ca5734cd307
Kosh1 <koshmanavt1@gmail.com>
2025-06-09 06:54:52 +0300

    Яндекс Метрика

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import { ThemeProvider } from "@/components/theme-provider"
 import { ScrollToTopButton } from "@/components/scroll-to-top-button"
 
+
 export const metadata: Metadata = {
   title: 'v0 App',
   description: 'Created with v0',
@@ -23,6 +24,23 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" />
         <link rel="manifest" href="/manifest.json" />
         <title>ИскИИ — AI генератор исковых заявлений</title>
+        <!-- Yandex.Metrika counter -->
+        <script type="text/javascript" >
+          (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          m[i].l=1*new Date();
+          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+          ym(102501372, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true
+          });
+        </script>
+        <noscript><div><img src="https://mc.yandex.ru/watch/102501372" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+        <!-- /Yandex.Metrika counter -->
       </head>
       <body>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
@@ -33,3 +51,5 @@ export default function RootLayout({
     </html>
   )
 }
+
+
