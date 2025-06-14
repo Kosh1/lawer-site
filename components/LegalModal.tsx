d7146d028974b8a568d106bf3a927c6de9b5aed8
Kosh1 <koshmanavt1@gmail.com>
2025-06-14 10:58:06 +0300

    Модалки для политик

---
+++ components/LegalModal.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import * as React from "react"
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog"
+
+export function LegalModal({
+  open,
+  onClose,
+  title,
+  children,
+}: {
+  open: boolean
+  onClose: () => void
+  title: string
+  children: React.ReactNode
+}) {
+  return (
+    <Dialog open={open} onOpenChange={onClose}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+        </DialogHeader>
+        <div className="prose max-w-none">{children}</div>
+      </DialogContent>
+    </Dialog>
+  )
+}
