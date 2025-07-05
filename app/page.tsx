ac07066007e010947cde6d5d39fb695125610340
Kosh1 <koshmanavt1@gmail.com>
2025-07-05 11:04:26 +0300

    Новый лендинг

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 export const dynamic = "force-dynamic";
 
-import LandingPageClient from "@/components/landing-page-client";
+//import LandingPageClient from "@/components/landing-page-client";
 import HeroSectionV2 from "@/components/hero-section-v2";
 
 export default function HomePage() {
   return (
     <main>
       <HeroSectionV2 />
-      <LandingPageClient />
     </main>
   );
 }
