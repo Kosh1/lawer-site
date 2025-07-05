2be1fb87f176af80e390f2190a4d3c8196b57ea6
Kosh1 <koshmanavt1@gmail.com>
2025-07-05 11:38:38 +0300

    Новый хедер

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ export const dynamic = "force-dynamic";
 
 //import LandingPageClient from "@/components/landing-page-client";
 import HeroSectionV2 from "@/components/hero-section-v2";
+import { Header } from "@/components/header";
 
 export default function HomePage() {
   return (
     <main>
+      <Header />
       <HeroSectionV2 />
     </main>
   );
